54e394e3789ee1cb8ac6b0710a4a519800f57361
Poupou <poupou-web3@protonmail.com>
2023-01-18 15:22:06 +0100

    Fix test by using the right test_wallet address.

--- src/test/flipside/FlipsideApiTest.py
+++ src/test/flipside/FlipsideApiTest.py
@@ -24,12 +24,12 @@ class FlipsideApiTest(unittest.TestCase):
 
     def test_get_string_address(self):
         string_add = self.flipside_api.get_string_address(self.list_unique_address)
-        expected = 'LOWER(\'0x76f69dcddd0593b0aff5fd3280c3433ddb68e0d2\'),LOWER(\'0x1bc5ebee4738fd95bd96751c45a90889f772e0f3\'),LOWER(\'0x3812801cbf0e41413db4835a5e36228ad45e32bf\'),LOWER(\'0x8f8a255c64ec0109092674a7219f4d10f625e788\'),LOWER(\'0x5e280efdb4dc45ec4aa73494f78b89a21741a9ba\'),LOWER(\'0xdffe6d135e4396f90ba66a1024bdeb6ef5df9276\'),LOWER(\'0xdcdbd2b5ca6430b2456850cbd2c164413852381a\'),LOWER(\'0xe9034bcd20119ee7563d145dc817820690afd5fb\')'
+        expected = 'LOWER(\'0x06cd8288dc001024ce0a1cf39caaedc0e2db9c82\'),LOWER(\'0x9be7d88cfd6e4b519cd9720db6de6e6f2c1ca77e\'),LOWER(\'0xf8bde71eb161bd83da88bd3a1003eef9ba0c7485\'),LOWER(\'0x1994bc4f630a373ffc3ecef84165cfb85e7f7820\'),LOWER(\'0x13ef1086cdfecc00e0f8f3b2ac2c600f297dc333\'),LOWER(\'0xb324b8ab8634a6c160361d34e672cec739ac55cd\'),LOWER(\'0x1b7a0da1d9c63d9b8209fa5ce98ac0d148960800\'),LOWER(\'0xe718bb18d8176659606b3d7d3f705906a9d3e1bd\')'
         self.assertEqual(expected, string_add)
 
     def test_get_eth_transactions_sql_query(self):
         sql = self.flipside_api.get_eth_transactions_sql_query(self.list_unique_address)
-        expected = 'WHERE FROM_ADDRESS IN (LOWER(\'0x76f69dcddd0593b0aff5fd3280c3433ddb68e0d2\'),LOWER(\'0x1bc5ebee4738fd95bd96751c45a90889f772e0f3\'),LOWER(\'0x3812801cbf0e41413db4835a5e36228ad45e32bf\'),LOWER(\'0x8f8a255c64ec0109092674a7219f4d10f625e788\'),LOWER(\'0x5e280efdb4dc45ec4aa73494f78b89a21741a9ba\'),LOWER(\'0xdffe6d135e4396f90ba66a1024bdeb6ef5df9276\'),LOWER(\'0xdcdbd2b5ca6430b2456850cbd2c164413852381a\'),LOWER(\'0xe9034bcd20119ee7563d145dc817820690afd5fb\'))'
+        expected = 'WHERE FROM_ADDRESS IN (LOWER(\'0x06cd8288dc001024ce0a1cf39caaedc0e2db9c82\'),LOWER(\'0x9be7d88cfd6e4b519cd9720db6de6e6f2c1ca77e\'),LOWER(\'0xf8bde71eb161bd83da88bd3a1003eef9ba0c7485\'),LOWER(\'0x1994bc4f630a373ffc3ecef84165cfb85e7f7820\'),LOWER(\'0x13ef1086cdfecc00e0f8f3b2ac2c600f297dc333\'),LOWER(\'0xb324b8ab8634a6c160361d34e672cec739ac55cd\'),LOWER(\'0x1b7a0da1d9c63d9b8209fa5ce98ac0d148960800\'),LOWER(\'0xe718bb18d8176659606b3d7d3f705906a9d3e1bd\'))'
         self.assertTrue(expected in sql)
 
     def test_execute_get_tx_eth(self):
