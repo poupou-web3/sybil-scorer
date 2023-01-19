be2770072087bca2d17e755ff12b371be0ff2c7a
Poupou <poupou-web3@protonmail.com>
2023-01-19 19:43:15 +0100

    WIP fix  tags extraction from multiple csv

--- src/main/extract_tags_labels.py
+++ src/main/extract_tags_labels.py
@@ -21,13 +21,15 @@ list_address = []
 for network in os.listdir(PATH_TO_TRANSACTIONS):
     print("Extracting transactions for network: ", network)
     path_to_export = os.path.join(PATH_TO_TRANSACTIONS, network)
-    df_address = pd.read_csv(
-        os.path.join(path_to_export, "transactions.csv"),
-        usecols=["from_address", "to_address"])
-    list_address.append(df_address.from_address.values)
-    list_address.append(df_address.to_address.values)
+    for file in os.listdir(path_to_export):
+        df_address = pd.read_csv(
+            os.path.join(path_to_export, file),
+            usecols=["from_address", "to_address"])
+        list_address.append(df_address.from_address.values)
+        list_address.append(df_address.to_address.values)
 
-list_unique_address = np.unique(np.concatenate(list_address))
+list_address = np.concatenate(list_address)
+list_unique_address = np.unique(list_address.astype(str))
 
 print("Start extracting tags")
 sql_tag = flipside_api.get_cross_chain_address_tags_sql_query(list_unique_address)
