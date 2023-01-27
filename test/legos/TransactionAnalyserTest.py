a5b9707bfd3e7148eea3abddaadd8625afa6fd36
Poupou <poupou-web3@protonmail.com>
2023-01-27 15:56:35 +0100

    test_transaction_similitude_true and remove wrong test

--- test/legos/TransactionAnalyserTest.py
+++ test/legos/TransactionAnalyserTest.py
@@ -34,10 +34,6 @@ class TransactionAnalyserTest(unittest.TestCase):
         self.assertFalse(self.tx_analyser.has_same_seed_naive(
             address))  # this one is false because it is not looking at the right address
 
-    def test_has_same_seed(self):
-        address = "0x000b94c47e4a8d7a70be12c50fc35722a7596972"
-        self.assertFalse(self.tx_analyser.has_same_seed(address))
-
     def test_has_same_seed_True(self):
         address = "0x000b94c47e4a8d7a70be12c50fc35722a7596972"
         self.assertTrue(self.tx_analyser.has_same_seed(address))
@@ -56,6 +52,13 @@ class TransactionAnalyserTest(unittest.TestCase):
         address = "0x000aa644afae99d06c9a0ed0e41b1e61beca958d"
         tx_sim = self.tx_analyser.transaction_similitude(address=address, algo_type="address_only", char_tolerance=0)
         self.assertEqual(0, tx_sim.shape[0])
+
+    def test_transaction_similitude_true(self):
+        address = "0xlcsad8bc3dfbe42d9a87686f67c69001a2006da4"  # copy of 0x000ad8bc3dfbe42d9a87686f67c69001a2006da4
+        address_lcs = pd.read_csv(os.path.join(self.path_to_test_add, "tx_analyser_address_lcs.csv"))
+        tx_analyser_lcs = TransactionAnalyser(self.df_tx, address_lcs)
+        tx_sim = tx_analyser_lcs.transaction_similitude(address=address, algo_type="address_only", char_tolerance=0)
+        self.assertEqual(52, tx_sim.loc['0x000ad8bc3dfbe42d9a87686f67c69001a2006da4', 'lcs'])
 
 
 if __name__ == '__main__':
