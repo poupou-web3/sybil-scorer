e7535b903ba8394216ec957bec7b7371974fab0f
Poupou <poupou-web3@protonmail.com>
2023-01-27 19:32:49 +0100

    Try with more os path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "sbscorer")
+
 project = 'Sybil Scorer'
 copyright = '2023, Poupou'
 author = 'Poupou'
