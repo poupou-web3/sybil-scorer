22f6adec229148d7fc4847e7814cde61138231ee
Poupou <108153089+poupou-web3@users.noreply.github.com>
2023-01-28 13:38:37 +0100

    Try new conf doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,19 +14,23 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # Source code dir relative to this file
+
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc',  # Core library for html generation from docstrings
+    'sphinx.ext.autosummary',  # Create neat summary tables
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon'
 ]
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 html_static_path = []
-
-numpydoc_show_class_members = False
