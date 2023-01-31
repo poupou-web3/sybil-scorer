fdecd5090508e86efc11d55ae2a457b76d08d728
Poupou <poupou-web3@protonmail.com>
2023-01-31 19:14:21 +0100

    Fix nan values in transaction similitude

--- sbscorer/sblegos/TransactionAnalyser.py
+++ sbscorer/sblegos/TransactionAnalyser.py
@@ -402,11 +402,17 @@ class TransactionAnalyser(object):
         """
         df_address_transactions.sort_values('block_timestamp', ascending=True, inplace=True)
         if algo_type == "address_only":
-            array_transactions = df_address_transactions.loc[:, ['from_address', 'to_address']] \
-                .replace(address, 'x').agg('-'.join, axis=1).values
+            try:
+                array_transactions = df_address_transactions.loc[:, ['from_address', 'to_address']].dropna().replace(
+                    address, 'x').agg('-'.join, axis=1).values
+            except Exception as e:
+                array_transactions = []
         elif algo_type == "address_and_value":
-            array_transactions = df_address_transactions.loc[:, ['from_address', 'value', 'to_address']] \
-                .replace(address, 'x').agg('-'.join, axis=1).values
+            try:
+                array_transactions = df_address_transactions.loc[:, ['from_address', 'value', 'to_address']].dropna() \
+                    .replace(address, 'x').agg('-'.join, axis=1).values
+            except Exception as e:
+                array_transactions = []
         else:
             raise ValueError("algo_type must be either address_only or address_and_value")
         return array_transactions
